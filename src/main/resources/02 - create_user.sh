18ef224e5d6656ef0961a8e2e632d9efb4f8a32b
Heber F Barra <heberferreirabarra@gmail.com>
2024-09-20 11:04:56 -0300

    Mudança na configuração e criação de usuário

--- src/main/resources/02 - create_user.sh
+++ src/main/resources/02 - create_user.sh
@@ -2,7 +2,7 @@
 set -e
 
 mysql --protocol=socket -uroot -p"$MYSQL_ROOT_PASSWORD" <<EOSQL
-CREATE USER IF NOT EXISTS 'usuario'@localhost IDENTIFIED BY 'SENHA_USUARIO';
-GRANT INSERT, SELECT, UPDATE, DELETE ON db_crud.* TO 'usuario'@localhost;
+CREATE USER IF NOT EXISTS 'usuario'@'%' IDENTIFIED BY '$SENHA_USUARIO';
+GRANT INSERT, SELECT, UPDATE, DELETE ON db_crud.* TO 'usuario'@'%';
 FLUSH PRIVILEGES;
 EOSQL
